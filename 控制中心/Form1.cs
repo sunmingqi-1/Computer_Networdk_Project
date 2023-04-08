f24fcb897f14ccb544e3deebb280253e99b70cee
sunmingqi <sun13624625009@163.com>
2023-04-08 16:20:09 +0800

    搭建控制中心界面框架

--- 控制中心/Form1.cs
+++ 控制中心/Form1.cs
@@ -1,10 +1,34 @@
+using Sunny.UI;
+using System.Reflection;
+
 namespace 控制中心
 {
-    public partial class Form1 : Form
+    public partial class Form1 : UIHeaderAsideMainFrame
     {
         public Form1()
         {
             InitializeComponent();
+            init_Main_Aside();
+        }
+        private void init_Main_Aside()
+        {
+            //// 页面1
+            //int pageIndex = 1000;
+            //Header.SetNodePageIndex(Header.Nodes[0], pageIndex);
+            //Header.SetNodeSymbol(Header.Nodes[0], 61451);
+            //TreeNode parent = Aside.CreateNode("模型", 61451, 24, pageIndex);
+
+            //// 页面2
+            //pageIndex = 2000;
+            //Header.SetNodePageIndex(Header.Nodes[1], pageIndex);
+            //Header.SetNodeSymbol(Header.Nodes[1], 61818);
+            //parent = Aside.CreateNode("绘图", 61818, 24, pageIndex);
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
